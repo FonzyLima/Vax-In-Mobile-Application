fc9e15d09463ad731a27bbae3f1322848a3f4064
beaberenguer <beatrice_berenguer@dlsu.edu.ph>
2021-08-23 02:59:47 +0800

    vaccination profile

---
+++ VaxInMobileApplioation/app/src/main/java/com/mobdeve/s11/lima/buendia/berenguer/vax_inmobileapplioation/UserVaccinationProfileActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s11.lima.buendia.berenguer.vax_inmobileapplioation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserVaccinationProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_vaccination_profile);
+    }
+}
