daecba642650724726bdaaed5037aab13f566149
beaberenguer <beatrice_berenguer@dlsu.edu.ph>
2021-08-23 00:05:54 +0800

    reg vaccine design

---
+++ VaxInMobileApplioation/app/src/main/java/com/mobdeve/s11/lima/buendia/berenguer/vax_inmobileapplioation/UserMainActivity.java
@@ -0,0 +1,13 @@
+package com.mobdeve.s11.lima.buendia.berenguer.vax_inmobileapplioation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+    }
+}
